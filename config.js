baffd1e0f47b1d63a699ae8b819b15dd52c6ac1e
Austyn Studdard <KingCosmic2016@gmail.com>
2018-07-25 05:13:56 -0500

    initial push

---
+++ config.js
@@ -0,0 +1,78 @@
+
+const config = {
+  token: process.env.TOKEN,
+
+  devId: process.env.DEVID,
+
+  settings: {
+    prefix: 'L!',
+    welcomeEnabled: true,
+    welcomeChannel: 'mainhall',
+    welcomeMessage: 'Hello {{user}}! Welcome to CinnamonBuns!'
+  },
+
+  // PERMISSION LEVEL DEFINITIONS.
+
+  permLevels: [
+    // This is the lowest permisison level, this is for non-roled users.
+    {
+      level: 0,
+      name: "User", 
+      // Don't bother checking, just return true which allows them to execute any command their
+      // level allows them to.
+      check: () => true
+    },
+
+    // This is your permission level, the staff levels should always be above the rest of the roles.
+    {
+      level: 2,
+      // This is the name of the role.
+      name: "Moderator",
+      // The following lines check the guild the message came from for the roles.
+      // Then it checks if the member that authored the message has the role.
+      // If they do return true, which will allow them to execute the command in question.
+      // If they don't then return false, which will prevent them from executing the command.
+      check: (message) => {
+        try {
+          const modRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.modRole.toLowerCase());
+          if (modRole && message.member.roles.has(modRole.id)) return true;
+        } catch (e) {
+          return false;
+        }
+      }
+    },
+
+    {
+      level: 3,
+      name: "Administrator", 
+      check: (message) => {
+        try {
+          const adminRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.adminRole.toLowerCase());
+          return (adminRole && message.member.roles.has(adminRole.id));
+        } catch (e) {
+          return false;
+        }
+      }
+    },
+
+    // This is the server owner.
+    {
+      level: 4,
+      name: "Bot Dev", 
+      // Simple check, if the guild owner id matches the message author's ID, then it will return true.
+      // Otherwise it will return false.
+      check: (message) => message.author.id === config.devId
+    },
+
+    // This is the server owner.
+    {
+      level: 5,
+      name: "Server Owner", 
+      // Simple check, if the guild owner id matches the message author's ID, then it will return true.
+      // Otherwise it will return false.
+      check: (message) => message.channel.type === "text" ? (message.guild.owner.user.id === message.author.id ? true : false) : false
+    },
+  ]
+}
+
+module.exports = config;
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,30 @@
+
+// This event executes when a new member joins a server. Let's welcome them!
+
+module.exports = (client, member) => {
+  // Load the guild's settings
+  const settings = client.config.settings;
+
+  // If welcome is off, don't proceed (don't welcome the user)
+  if (!settings.welcomeEnabled) return;
+
+  // Replace the placeholders in the welcome message with actual data
+  const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
+
+  const embed = {
+    "title": welcomeMessage,
+    "color": 16663691,
+    "timestamp": Date.now(),
+    "footer": {
+      "text": "Enjoy your stay :3"
+    },
+    "image": {
+      "url": "https://cdn.discordapp.com/attachments/471600179662028802/471615977101197322/9c0b6c1a89547a4e53964a5e865ad5b3-700.jpg"
+    }
+  };
+  channel.send({ embed });
+
+  // Send the welcome message to the welcome channel
+  // There's a place for more configs here.
+  member.guild.channels.find("name", settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+};
