16895f19c3be256ce78baa407d6ca9fddfb9340d
Austyn Studdard <KingCosmic2016@gmail.com>
2018-07-26 11:28:50 -0500

    added changing statuses

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
+const games = ['Resident Evil 7', 'Crush Crush', 'with Lego!', 'with Sayori', 'with Crayons!', 'Katawa Shoujo', 'Doki Doki Literature Club'];
 
 module.exports = async client => {
   // Log that the bot is online.
@@ -5,4 +6,8 @@ module.exports = async client => {
 
   // Make the bot "play the game" which is the help command with default prefix.
   client.user.setActivity(`${client.config.settings.prefix}help`, { type: "PLAYING" });
+
+  setInterval(() => {
+    client.user.setActivity(games[Math.floor(Math.random() * games.length)], { type: 'PLAYING' });
+  })
 };
