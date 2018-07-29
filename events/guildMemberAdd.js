b77e12d54e98a4363e0dfb3dbbd180e50594ccdc
Austyn Studdard <KingCosmic2016@gmail.com>
2018-07-29 08:27:41 -0500

    trying a fix for the welcome embed

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,11 +8,8 @@ module.exports = (client, member) => {
   // If welcome is off, don't proceed (don't welcome the user)
   if (!settings.welcomeEnabled) return;
 
-  // Replace the placeholders in the welcome message with actual data
-  const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user);
-
   const guildEmbed = {
-    "title": welcomeMessage,
+    "title": `Hello ${member}! Welcome to CinnamonBuns!`,
     "color": 16663691,
     "timestamp": new Date().toISOString(),
     "footer": {
@@ -29,7 +26,7 @@ module.exports = (client, member) => {
   .then(() => {
 
     const dmEmbed = {
-      "title": "Welcome {{user}}! Welcome to cinnamonBuns!".replace("{{user}}", member.user),
+      "title": `Welcome ${member}! Welcome to cinnamonBuns!`,
       "description": "Before you begin chatting there's a couple things you could do first to make being in this server the best experience possible!\nPlease read the Rules so you don't accidently break any!\nIf you want a custom colour you can get a number of different colours if you say =rank in Botchannel!\n\nOnce you've done this you're ready to start chatting, if you have any questions please send them to the server owner Natyori#0001\n\nAlso if you accidently leave the server or want to invite a friend then here's a permanent invite link!\n\nhttps://discord.gg/q55yXMe\n\nIf you invite somebody to the server, you can acquire the Supporter role!\nPlease ask a mod/admin to acquire it after you have done this!",
       "color": 12011097,
       "timestamp": "2018-07-25T18:14:20.238Z",
